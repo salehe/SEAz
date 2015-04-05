c4067b62c140a31192d936cf43d238dc70d9b604
epcpu <mnheydari@yahoo.com>
2015-04-05 12:17:19 +0430

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,21 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+    
+    private static void testSubtract()
+    {
+        int a = 5;
+        int b = 6;
+        int expected = -1;
+        BasicMath bm = new BasicMath();
+        int result = bm.subtract(a, b);
+        if (result == expected)
+        {
+            System.out.println("Subtract: OK");
+        }
+        else
+        {
+            System.out.println("Subtract: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
 }
