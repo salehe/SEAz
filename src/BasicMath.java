61a054ff3d71ee36cb44b4a0e15e56cb46397ad2
epcpu <mnheydari@yahoo.com>
2015-04-05 12:12:35 +0430

    Update BasicMath.java

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -5,7 +5,10 @@ class BasicMath
     {
         return a + b;
     }
-
+    
+    public subtract(int a, int b) {
+	return a - b;    	
+    }
     // TODO: subtract
 	// TODO: multiply
 	// TODO: divide
