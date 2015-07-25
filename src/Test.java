382aa3ce8bdd0b12b0720adaadcf1b3119270bde
Reyhane93 <Reyhane93@users.noreply.github.com>
2015-07-25 00:17:11 -0700

    Update Test.java
    test division

--- src/Test.java
+++ src/Test.java
@@ -2,23 +2,23 @@ class Test
 {
     public static void main(String[] args)
     {
-        testSum();
+        testDivision();
     }
 
-    private static void testSum()
+    private static void testDivision()
     {
-        int a = 5;
+        int a = 30;
         int b = 6;
-        int expected = 11;
+        int expected = 5;
         BasicMath bm = new BasicMath();
-        int result = bm.sum(a, b);
+        int result = bm.divide(a, b);
         if (result == expected)
         {
-            System.out.println("Sum: OK");
+            System.out.println("Divide: OK");
         }
         else
         {
-            System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+            System.out.println("Divide: Failed, expected = " + expected + ", result = " + result);
         }
     }
 }
