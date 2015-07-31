2ce1dabf493a47dba5a0c8e2a8e9644fb4456219
Reyhane93 <Reyhane93@users.noreply.github.com>
2015-07-31 12:56:07 -0700

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,21 @@ class Test
             System.out.println("Divide: Failed, expected = " + expected + ", result = " + result);
         }
     }
+    
+    private static void testSum()
+    {
+        int a = 30;
+        int b = 6;
+        int expected = 36;
+        BasicMath bm = new BasicMath();
+        int result = bm.sum(a, b);
+        if (result == expected)
+        {
+            System.out.println("Sum: OK");
+        }
+        else
+        {
+            System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
 }
