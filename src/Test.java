b3ef763814d6cf4ca3e485841903381eca2566a9
zmaster94 <zeynab0amini@gmail.com>
2017-07-31 16:51:13 +0430

    Write log

--- src/Test.java
+++ src/Test.java
@@ -21,4 +21,21 @@ class Test
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
         }
     }
+
+    private static void testSubtract()
+    {
+        int a = 8;
+        int b = 3;
+        int expected = 5;
+        BasicMath bm = new BasicMath();
+        int result = bm.substract(a, b);
+        if (result == expected)
+        {
+            System.out.println("Subtract: OK");
+        }
+        else
+        {
+            System.out.println("Subtract: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
 }
